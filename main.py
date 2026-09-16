b23fa71ea549013745b9c0d8555f381089bb20c8
joao-p-garcia <crafteternum@gmail.com>
2026-09-16 16:58:02 -0300

    initial tests

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+
+cno_totais = pd.read_csv(r'dados_raw\cno_totais.csv', encoding='latin-1')
+cno_cnaes = pd.read_csv(r'dados_raw\cno_cnaes.csv', encoding='latin-1')
+cno_vinculos = pd.read_csv(r'dados_raw\cno_vinculos.csv', encoding='latin-1')
+cno_areas = pd.read_csv(r'dados_raw\cno_areas.csv', encoding='latin-1')
+
+
+print("prant")
